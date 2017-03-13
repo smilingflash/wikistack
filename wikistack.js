9faca7241411a10b82494fa812a98843b48463d7
Fanny Jiang <jiangf@bu.edu>
2017-03-13 11:02:12 -0400

    completed syncing models

--- wikistack.js
+++ wikistack.js
@@ -5,11 +5,42 @@ var path = require('path');
 var wikistack = express();
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var routes = require('./routes')
+var morgan = require('morgan');
+var models = require('./models');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    // logging: false
+});
 
 
 // point nunjucks to the directory containing templates and turn off caching; configure returns an Environment instance, which we'll want to use to add Markdown support later.
-var env = nunjucks.configure('views', {noCache: true});
-wikistack.set('view engine', 'html'); // have res.render work with html files
+var env = nunjucks.configure('views', { noCache: true });
+
 wikistack.engine('html', nunjucks.render); // when res.render works with html files, have it use nunjucks to do so
+wikistack.set('view engine', 'html'); // have res.render work with html files
+
+
+// middleware logger
+wikistack.use(morgan('dev'));
+
+// body parser
+wikistack.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+wikistack.use(bodyParser.json()); // would be for AJAX requests
 
 wikistack.use(express.static(path.join(__dirname, '/public')));
+
+// router to serve up index.html
+wikistack.use('/', routes);
+
+// syncing the our models page to the database
+models.User.sync({})
+    .then(function() {
+        return models.Page.sync({})
+    })
+    // server
+    .then(function() {
+        server.listen(3000, function() {
+            console.log('Server is listening on port 3001!');
+        });
+    })
+    .catch(console.error);
