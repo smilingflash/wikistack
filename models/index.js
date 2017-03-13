bdae0b6a381db0443cf98f069d1beeb246430025
Fanny Jiang <jiangf@bu.edu>
2017-03-13 12:57:10 -0400

    working on post

--- models/index.js
+++ models/index.js
@@ -1,55 +1,68 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack', {
-  logging: false
+    logging: false
 }); // connects to currently-running db process
 
 const Page = db.define('page', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  urlTitle: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  status: {
-    type: Sequelize.ENUM('open', 'closed'),
-    // defaultValue: 'closed'
-  },
-  date: {
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.NOW
-  }
-}, {
-  getterMethods: {
-    route: function(){
-      return '/wiki/' + this.urlTitle;
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed'),
+        // defaultValue: 'closed'
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
     }
-  }
+}, {
+    getterMethods: {
+        route: function() {
+            return '/wiki/' + page.urlTitle;
+        }
+    }
+}, {
+    hooks: {
+        beforeValidate: function(page, title) {
+            if (title) {
+                // Removes all non-alphanumeric characters from title
+                // And make whitespace underscore
+                page.urlTitle = title.replace(/\s+/g, '_').replace(/\W/g, '');
+            } else {
+                // Generates random 5 letter string
+                page.urlTitle = Math.random().toString(36).substring(2, 7);
+            }
+        }
+    }
 });
 
 const User = db.define('user', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true    // doesn't allow empty strings
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true // doesn't allow empty strings
+        }
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true // checks for email format (foo@bar.com)
+        }
     }
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true   // checks for email format (foo@bar.com)
-    }
-  }
 });
 
 module.exports = {
-  Page: Page,
-  User: User
+    Page: Page,
+    User: User
 };
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
 router.get('/wiki', function(req, res, next) {
-  res.redirect('/');
+    res.redirect('/');
+});
+
+// router.post('/wiki', function(req, res, next) {
+//     res.send('got to POST /wiki/');
+// });
+
+router.get('/wiki/add', function(req, res, next) {
+    res.render('addpage');
 });
 
 router.post('/wiki', function(req, res, next) {
-  res.send('got to POST /wiki/');
-});
 
-router.get('/wiki/add', function(req, res, next) {
-  res.render('addpage');
+    // STUDENT ASSIGNMENT:
+    // add definitions for `title` and `content`
+
+    var page = Page.build({
+        title: Page.title,
+        content: Page.content
+    });
+
+    // STUDENT ASSIGNMENT:
+    // make sure we only redirect *after* our save is complete!
+    // note: `.save` returns a promise or it can take a callback.
+    page.save()
+        // -> after save -> res.redirect('/');
+    res.redirect('/')
 });
 
 module.exports = router;
